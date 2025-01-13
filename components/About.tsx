57655a3573df5c7387f62d560eb3087a469facd5
JavaNi80 <71756562+Tebing27@users.noreply.github.com>
2025-01-13 20:03:33 +0700

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 
 const About = () => {
   return (
-    <section id="tentang" className="py-20 px-4 bg-white">
+    <section id="profil" className="py-20 px-4 bg-white">
       <div className="max-w-7xl mx-auto">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
